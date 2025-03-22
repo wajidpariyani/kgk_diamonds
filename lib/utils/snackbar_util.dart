a732e6dc93ce8a55fc1a2ecf88e87d5dff3d7f4a
Wajid Pariyani <wajidpariyani@Wajids-MacBook-Pro.local>
2025-03-22 18:47:31 +0530

    snacbar close

--- lib/utils/snackbar_util.dart
+++ lib/utils/snackbar_util.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class SnackbarUtil {
   static void showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text(message)),
     );
